3f903b6ef69d8a0086996c4a6ef683484190c692
nor-03m <minori03.m@gmail.com>
2021-02-12 17:29:03 +0900

    add todo number

--- vue-todolist/src/components/ToDo.vue
+++ vue-todolist/src/components/ToDo.vue
@@ -2,7 +2,12 @@
     <div class="todo">
         <div>        
             <li><input type="text" v-model="ToDo"></li>
+            <li><input type="text" v-model="DeadLine"></li>
             <li><v-btn type="submit" v-on:click="addToDo()">add Task</v-btn></li>
+        </div>
+        <div>
+            <li><v-btn type="submit" v-on:click="allToDo()">all Task</v-btn></li>
+            <li>{{ num }}</li>
         </div>
     </div>
 </template>
@@ -16,28 +21,56 @@ export default {
         return{
             db: null,
             ToDo: '',
+            DeadLine: '',
+            DeleteToDo: '',
+            num: NaN,
         }
     },
+
     created: function(){
         this.db = firebase.firestore()
     },
+
+    mounted: function(){
+        var _this = this;
+
+        _this.db.collection('todos').get().then(snap => {
+            _this.num = snap.size;
+        })
+    },
+
     methods: {
+
         addToDo: function(){
-            var _this = this
+            var _this = this;
+
+            if(_this.ToDo=="" || _this.DeadLine==""){
+                return;
+            }
+
+            _this.num++;
 
             // todos コレクションにドキュメントを追加
-            this.db.collection('todos').doc('0OwOfIWnpks82XeANMw9').set({
-                ToDo: _this.ToDo,
-                
-            }, {merge: true})
-            .then(function(){
-                // 追加に成功したら、name を空にする
-                _this.ToDo = ''
-            })
-            .catch(function(){
+            _this.db.collection('todos').doc('todo' + String(_this.num)).set({
+                todo: _this.ToDo,
+                deadline: _this.DeadLine
+            }).then(function(){
+                // 追加に成功したら、ToDo、DeadLine を空にして、num を1増やす
+                _this.ToDo = '',
+                _this.DeadLine = ''
+            }).catch(function(){
                 // エラー時の処理
             })
-        },   
+        },
+
+        allToDo: function(){
+            this.db.collection('todos').get().then(function(querySnapshot){
+                querySnapshot.forEach(function(doc){
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log(doc.id, "=>", doc.data());
+                })
+            })
+        },
     },
     computed: {
     },
